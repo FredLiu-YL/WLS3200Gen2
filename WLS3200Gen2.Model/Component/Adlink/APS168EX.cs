263744c7194e686dca5cfa941899ff0c3964154d
zhengye_lin@yuanyu.tw <zhengye_lin@yuanyu.tw>
2023-12-29 12:02:48 +0800

    新增Adlink軸卡

---
+++ WLS3200Gen2.Model/Component/Adlink/APS168EX.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WLS3200Gen2.Model.Component.Adlink
+{
+    /// <summary>
+    /// Adlink-PCI(e)7856 控制卡 Library
+    /// </summary>
+    public static class APS168Lib
+    {
+
+
+    }
+}
